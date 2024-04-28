4c53cae7c1a0bd7f9594abc3f1dac1882986476d
hamza halawa <69251476+hamza-halawa2020@users.noreply.github.com>
2024-04-28 15:31:29 +0300

    sdf ds

--- front-academy/src/app/sign/services/sign.service.ts
+++ front-academy/src/app/sign/services/sign.service.ts
@@ -33,7 +33,7 @@ export class SignService {
   }
 
   logout() {
-    this.cookieService.deleteAll();
-    this.router.navigate(['']);
+    this.cookieService.delete('token');
+    this.router.navigate(['/login']);
   }
 }
