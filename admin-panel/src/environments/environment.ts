b766edf6611f8a1f864489b93ab275cd204de984
hamza halawa <69251476+hamza-halawa2020@users.noreply.github.com>
2024-05-01 17:08:04 +0300

    سييسل

--- admin-panel/src/environments/environment.ts
+++ admin-panel/src/environments/environment.ts
@@ -1,8 +1,8 @@
-// export const environment = {
-//   backEndUrl: 'back-academy.rimansan.net/public/api/',
-//   imgUrl: 'back-academy.rimansan.net/',
-// };
 export const environment = {
-  backEndUrl: 'http://127.0.0.1:8000/api/',
-  imgUrl: 'http://127.0.0.1:8000/',
+  backEndUrl: 'back-academy.rimansan.net/public/api/',
+  imgUrl: 'back-academy.rimansan.net/',
 };
+// export const environment = {
+//   backEndUrl: 'http://127.0.0.1:8000/api/',
+//   imgUrl: 'http://127.0.0.1:8000/',
+// };
